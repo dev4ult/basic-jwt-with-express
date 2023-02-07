a5ce7f294e909bfcb8bccbe360cd48e2f55e863f
dev4ult <nibrasbiasaaja@gmail.com>
2023-02-07 21:34:51 +0700

    more config

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 connectDatabase();
 
